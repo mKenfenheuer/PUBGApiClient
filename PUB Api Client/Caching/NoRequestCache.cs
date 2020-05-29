44a0e6abfe58db9302f0ed35f31b097c4c52f048
Maximilian Kenfenheuer <maximilian.kenfenheuer@ksol.it>
2020-05-29 20:47:16 +0200

    Create NoRequestCache.cs

---
+++ PUB Api Client/Caching/NoRequestCache.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PUBG.ApiClient.Caching
+{
+    public class NoRequestCache : IRequestCache
+    {
+        #pragma warning disable CS1998
+        public async Task AddResponseToCache(string requestUrl, string response, DateTime requestTime)
+        {
+            //Do nothing
+        }
+
+        public async Task<string> TryGetResponseFromCache(string requestUrl, TimeSpan maxAge)
+        {
+            //Always return null to disable caching
+            return null;
+        }
+        #pragma warning restore CS1998
+    }
+}
